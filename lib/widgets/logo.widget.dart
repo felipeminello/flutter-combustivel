28d970f0a321e6c859f1bdc26ecd63e3fff7f690
Felipe Minello <felipeminello@gmail.com>
2020-05-03 15:11:53 -0300

    first commit

---
+++ lib/widgets/logo.widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Logo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 40,
+        ),
+        Image.asset(
+          'assets/images/aog-white.png',
+          height: 80,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          'Álcool ou Gasolina',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Big Shoulders Display',
+          ),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
